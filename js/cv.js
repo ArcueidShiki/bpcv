eac3db3d22ae3ce805bfc003e38929954bbbf8ed
ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>
2025-03-30 22:58:13 +0800

    adjust cv content css need to be modified
    Signed-off-by: ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>

--- js/cv.js
+++ js/cv.js
@@ -1,25 +1,24 @@
 // Default language
 let currentLanguage = "en";
 
-function LoadConfig(language) {
-  const filePath = `js/langs/${language}.json`;
+function loadContactInfo(data) {
+  const contactInfo = data.contactInfo;
 
-  // Fetch the JSON file
-  $.getJSON(filePath, function (data) {
-    console.log(`Loaded file: ${filePath}`);
-    console.log(data);
-
-    const contactInfo = data.contactInfo;
-
-    // Dynamically generate the HTML for the contact information
-    const contactHTML = `
+  // Dynamically generate the HTML for the contact information
+  const contactHTML = `
       <div class="contact-card">
-        <img class="profile-picture" src="${contactInfo.profilePicture}" width="100px" alt="${contactInfo.name}'s Profile Picture">
-        <h1 class="name">${contactInfo.name}</h1>
+        <img class="profile-picture" src="${
+          contactInfo.profilePicture
+        }" width="100px" alt="${contactInfo.name}'s Profile Picture">
+        <h2 class="name">${contactInfo.name}</h1>
         <h2 class="position">${contactInfo.position}</h2>
         <div class="contact-details">
-          <p><strong>Phone:</strong> ${contactInfo.phone.map(phone => `<a href="tel:${phone}">${phone}</a>`).join(", ")}</p>
-          <p><strong>Email:</strong> ${contactInfo.email.map(email => `<a href="mailto:${email}">${email}</a>`).join(", ")}</p>
+          <p><strong>Phone:</strong> ${contactInfo.phone
+            .map((phone) => `<a href="tel:${phone}">${phone}</a>`)
+            .join(", ")}</p>
+          <p><strong>Email:</strong> ${contactInfo.email
+            .map((email) => `<a href="mailto:${email}">${email}</a>`)
+            .join(", ")}</p>
           <div class="websites">
             <strong>Websites:</strong>
               ${contactInfo.websites
@@ -41,43 +40,25 @@ function LoadConfig(language) {
         </div>
       </div>
     `;
-    $("#basic-info").html(contactHTML);
+  $("main").append(contactHTML);
+}
 
-    // Dynamically generate the HTML for the summary
-    if (data.profile)
-    {
-        const title = `<h2> ${data.profile.title || "Profile"}</h2>`;
-        const profileHTML = `<p>${data.profile.content}</p>`;
-        $("#profile").html(title);
-        $("#profile").html(profileHTML);
-    }
+function LoadProfile(data) {
+  const profileHTML = `<h2> ${data.profile.title || "Profile"}</h2><hr/>
+                        <p class="profile">${data.profile.content}</p>`;
+  $("main").append(profileHTML);
+}
 
+function LoadWorkExperience(data) {
+  const title = ``;
 
-    if (data.education) {
-        const title = `<h2> ${data.education.title || "Education"}</h2>`;
-      const educationHTML = data.education.content
-        .map(
-          (edu) => `
-          <div class="education">
-            <h3>
-                <img src="${edu.logo}" alt="edu logo" width="50px"> <span>${edu.institution}</span> <span>${edu.location}</span>
-            </h3>
-            <h3><span>${edu.degree}</span> <span>${edu.year}</span></h3>
-          </div>
-        `
-        )
-        .join("");
-      $("#education").html(title);
-      $("#education").html(educationHTML);
-    }
-
-    if (data.workexperience) {
-        const title = `<h2> ${data.workexperience.title || "Work Experience"}</h2>`;
-
-      const experienceHTML = data.workexperience.content
-        .map(
-          (exp) => `
-          <div class="experience-item">
+  const experienceHTML = `
+    <h2> ${data.workexperience.title || "Work Experience"}</h2><hr/>
+    <div class="experiences">
+    ${data.workexperience.content
+      .map(
+        (exp) => `
+          <div class="experience">
             <h3>${exp.position} - ${exp.company}</h3>
             <p>${exp.duration}</p>
             <p>${exp.location}</p>
@@ -85,48 +66,92 @@ function LoadConfig(language) {
             <p>${exp.description}</p>
           </div>
         `
-        )
-        .join("");
-      $("#work-experience").html(title);
-      $("#work-experience").html(experienceHTML);
-    }
+      )
+      .join("")}
+      </div>`;
+  $("main").append(experienceHTML);
+}
 
-    if (data.skills) {
-        const title = `<h2> ${data.skills.title || "Skills"}</h2>`;
-      const skillsHTML = data.skills.content
-        .map((skillset) => `<strong>${skillset.category}</strong>
-            <ul>${skillset.items.map((item) => `<li><span>${item.name}</span><img src="${item.iconUrl} alt="logo" width="20px" "></li>`).join("")}</ul>
-        `)
-        .join("");
-      $("#skills").html(title);
-      $("#skills").html(`<ul>${skillsHTML}</ul>`);
-    }
-
-    if (data.projects) {
-        const title = `<h2> ${data.projects.title || "Projects"}</h2>`;
-      const projectsHTML = data.projects.content
-        .map(
-          (project) => `
-          <div class="project-item">
-            <h3><a href="${project.link}" target="_blank">${project.name}</a></h3>
-            <p>${project.description}</p>
-            <ul>${project.technologies.map((tech) => `<li>${tech}</li>`).join("")}</ul>
+function LoadEducation(data) {
+  const educationHTML = `
+  <h2> ${data.education.title || "Education"}</h2><hr/>
+  <div class="educations">
+  ${data.education.content
+    .map(
+      (edu) => `
+          <div class="education">
+            <h3>
+                <img src="${edu.logo}" alt="edu logo" width="50px"> <span>${edu.institution}</span> <span>${edu.location}</span>
+            </h3>
+            <h3><span>${edu.degree}</span> <span>${edu.year}</span></h3>
           </div>
         `
-        )
-        .join("");
-      $("#projects").html(title);
-      $("#projects").html(projectsHTML);
-    }
+    )
+    .join("")}
+    </div>`;
+  $("main").append(educationHTML);
+}
 
-    if (data.languages) {
-        const title = `<h2> ${data.languages.title || "Languages"}</h2>`;
-      const languagesHTML = data.languages.content
-        .map((lang) => `<li>${lang.language}:${lang.proficiency}</li>`)
-        .join("");
-      $("#languages").html(title);
-      $("#languages").html(`<ul>${languagesHTML}</ul>`);
-    }
+function LoadSkills(data) {
+  const skillsHTML = `
+  <h2> ${data.skills.title || "Skills"}</h2><hr/>
+  <div class="skills">
+    ${data.skills.content
+        .map(
+        (skillset) => `
+        <div class="skillset">
+                <strong>${skillset.category}</strong>
+                <ul>${skillset.items.map((item) =>
+                    `<li><span>${item.name}</span><img src="${item.iconUrl}" alt="logo" width="20px"></li>`)
+                    .join("")}
+                </ul>
+        </div>`)
+        .join("")}
+    </div>`;
+  $("main").append(skillsHTML);
+}
+
+function LoadProjects(data) {
+  const projectsHTML = `
+  <h2> ${data.projects.title || "Projects"}</h2><hr/>
+    <div class="projects">
+        ${data.projects.content
+        .map(
+            (project) => `
+                <div class="project">
+                    <h3><a href="${project.link}" target="_blank">${project.name}</a></h3>
+                    <p>${project.description}</p>
+                    <ul>
+                        ${project.technologies.map((tech) => `<li>${tech}</li>`).join("")}
+                    </ul>
+                </div>`).join("")}
+    </div>`;
+  $("main").append(projectsHTML);
+}
+
+function LoadLanguages(data) {
+  const languagesHTML = `
+  <h2> ${data.languages.title || "Languages"}</h2>
+    <div class="languages">
+        ${data.languages.content.map((lang) => `
+            <li>${lang.language}:${lang.proficiency}</li>`
+        ).join("")}
+    </div>`;
+  $("main").append(languagesHTML);
+}
+
+function LoadConfig(language) {
+  const filePath = `js/langs/${language}.json`;
+
+  // Fetch the JSON file
+  $.getJSON(filePath, function (data) {
+    loadContactInfo(data);
+    LoadProfile(data);
+    LoadWorkExperience(data);
+    LoadEducation(data);
+    LoadSkills(data);
+    LoadProjects(data);
+    LoadLanguages(data);
   });
 }
 
